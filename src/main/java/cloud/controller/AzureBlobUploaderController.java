243a86131c624939e4512f7322a9f7468e2c6ec3
Harshit <Harshit@SEZLAP-18771.india.rsystems.com>
2025-11-03 19:14:05 +0530

    added deploy also

---
+++ src/main/java/cloud/controller/AzureBlobUploaderController.java
@@ -0,0 +1,16 @@
+package cloud.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/blob")
+public class AzureBlobUploaderController {
+    //@GetMapping("/presign-url")
+    //ResponseEntity<> getPreSignUrl(){
+
+    //}
+}
